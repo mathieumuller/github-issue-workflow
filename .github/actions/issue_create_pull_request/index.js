e37cd61caff9a2fb018c5692be6cd372d9c31031
mathieumuller <mathieu.muller1006@gmail.com>
2020-10-09 23:15:17 +0200

    cecefce

--- .github/actions/issue_create_pull_request/index.js
+++ .github/actions/issue_create_pull_request/index.js
@@ -24,8 +24,13 @@ async function createPullRequest() {
     if (!hasLabel(labelToListen)) {
         return;
     }
-
-    let issue = await getIssue();
+console.log(payload);
+    let issue = await getIssue(),
+    author = payload.sender.login,
+    milestone = issue.milestone.title,
+    subject = stringToSlug(issue.title),
+    type = await getType(),
+    name = '[' +type+ '][' +milestone+ '] '+ subject;
     console.log(issue);
 }
 
@@ -46,15 +51,16 @@ async function hasLabel(label) {
     return labelExists;
 }
 
-function addLabel(label) {
-    octokit.issues.addLabels({
-        owner: repositoryOwner,
-        repo: repositoryName,
-        issue_number: issueNumber,
-        labels: [label]
-    });
-}
- async function getIssue()
+// function addLabel(label) {
+//     octokit.issues.addLabels({
+//         owner: repositoryOwner,
+//         repo: repositoryName,
+//         issue_number: issueNumber,
+//         labels: [label]
+//     });
+// }
+
+async function getIssue()
  {
     let { data: issue } = await octokit.issues.get({
         owner: repositoryOwner,
@@ -65,7 +71,7 @@ function addLabel(label) {
       return issue;
  }
 
-async function removeStateLabels() {
+async function getType() {
     // get all the current labels of the issue
     let { data: currentLabels } = await octokit.issues.listLabelsOnIssue({
         owner: repositoryOwner,
@@ -75,31 +81,46 @@ async function removeStateLabels() {
 
     // and remove those with the 'State:' prefix
     currentLabels.forEach(function(currentLabel) {
-        if (currentLabel.name.substring(0, stateLabelPrefix.length) === stateLabelPrefix) {
-            removeLabel(currentLabel.name, issueNumber);
+        if (currentLabel.name.substring(0, typeLabelPrefix.length) === typeLabelPrefix) {
+            type = currentLabel.name.substring(typeLabelPrefix.length);
         }
     });
+
+    console.log(type);
+
+    return type;
 }
 
-async function removeLabel(label) {
-    octokit.issues.removeLabel({
-        owner: repositoryOwner,
-        repo: repositoryName,
-        issue_number: issueNumber,
-        name: label
-    });
-}
+// async function removeLabel(label) {
+//     octokit.issues.removeLabel({
+//         owner: repositoryOwner,
+//         repo: repositoryName,
+//         issue_number: issueNumber,
+//         name: label
+//     });
+// }
 
-async function getColumn() {
-    let columnId = projectCard.column_id;
-    
-    let { data: column } = await octokit.projects.getColumn({
-      column_id: columnId,
-    });
 
-    return column;
-}
 
 function basename(path) {
     return path.split('/').reverse()[0];
- }
+}
+
+function stringToSlug (str) {
+    str = str.replace(/^\s+|\s+$/g, ''); // trim
+    str = str.toLowerCase();
+  
+    // remove accents, swap ñ for n, etc
+    var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+    var to   = "aaaaeeeeiiiioooouuuunc------";
+    for (var i=0, l=from.length ; i<l ; i++) {
+        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+    }
+
+    str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+        .replace(/\s+/g, '-') // collapse whitespace and replace by -
+        .replace(/-+/g, '-'); // collapse dashes
+
+    return str;
+}
+
