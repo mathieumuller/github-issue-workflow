02cb910fb92c4965eebc616e3fbeddbbebdda51e
mathieumuller <mathieu.muller1006@gmail.com>
2020-10-16 10:00:36 +0200

    drfh

--- .github/actions/issue_create_pull_request/index.js
+++ .github/actions/issue_create_pull_request/index.js
@@ -23,7 +23,7 @@ try {
 async function createPullRequest() {
     // only create the pull request when the issue has been moved into the "in progress" column
     //let auth = await octokit.users.getAuthenticated();
-    console.log(github);
+    console.log(core);
     let columnName = await getColumnName();
     console.log(columnName);
     if(columnName !== core.getInput('triggerColumn')) {
