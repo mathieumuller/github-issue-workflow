49a747dd393bb9b349aa3cfb8b72d6727a607888
mathieumuller <mathieu.muller1006@gmail.com>
2020-10-23 15:06:41 +0200

    srfg

---
+++ .github/actions/create_child pull_request/index.js
@@ -0,0 +1,10 @@
+const tools = require('../tools.js'),
+    config = require('../config.js'),    
+    core = require('@actions/core'),    
+    github = require('@actions/github'),
+    token = core.getInput('token'),
+    context = github.context,
+    octokit = github.getOctokit(token),
+    payload = context.payload;
+
+console.log(payload);
